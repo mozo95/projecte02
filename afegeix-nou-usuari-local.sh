29033510c3056af89d9108f280fa6144076406af
mozo95 <57413382+mozo95@users.noreply.github.com>
2019-11-19 15:57:22 +0100

    Update afegeix-nou-usuari-local.sh

--- afegeix-nou-usuari-local.sh
+++ afegeix-nou-usuari-local.sh
@@ -1,63 +1,46 @@
-# Display the username, password, and the host where the user was
-created.
+#display the username, password, and the host where the user was
+#created.
 
 #!/bin/bash
-# If the user doesn't supply at least one argument, then give them help.
-usage() {
-# The first parameter is the user name.
-# The rest of the parameters are for the account comments.
-  echo "Uso: ${0} [-u USERNAME] [-c]" >&2
-  echo 'Script para creacion de usuario.' >&2
-  echo '  -u USER    Nombre del usuario.' >&2
-  echo '  -c         Comentario.' >&2
-  exit 1
-}
-
-# Generate a password.
-password() {
-  PASSWORD=$(date | sha265sum)
-}
-
 # Make sure the script is being executed with superuser privileges.
-if [[ "${UID}" -eq 0 ]]
+if [[ "${UID}" -eq 1 ]]
 then
-  while getopts ":u:c:" OPTION
-  do
-    case ${OPTION} in
-      u)
-        u=${OPTARG}
-        ;;
-      c)
-        c=${OPTARG}
-        ;;
-      *)
-        usage
-        ;;
-    esac
-  done
-  # Create the user.
-  # Force password change on first login.
-  useradd -c "${c}" -m ${u} -e
-  # Check to see if the useradd command succeeded.
-  if [[ "${?}" -ne 0 ]]
-  then
-    echo 'La creación de usuario ha sufrido un error'
+        echo 'No tienes privilegios para ejecutar este script.'
     exit 1
-  fi
-  # Change the password.
-  echo $u":"$PASSWORD | chpasswd
-  # Check to see if the passwd command succeeded.
-  if [[ "${?}" -ne 0 ]]
-  then
-    echo 'La asignacion de contraseña ha sufrido un error'
-    exit 1
-  fi
-  # Display the username, password, and the host where the user was created.
-  echo $u
-  echo $PASSWORD
-  echo $HOSTNAME
-else
-  echo 'No tienes privilegios para ejecutar este script.'
-  exit 1
 fi
 
+# If the user doesn't supply at least one argument, then give them help.
+# The first parameter is the user name.
+# The rest of the parameters are for the account comments.
+if [[ "$#" -lt 1 ]]
+then
+	echo "Este script requiere parametros por favor introducelos:"
+	echo "EJEMPLO: ${0} [USUARIO] [COMENTARIO]"
+	exit 1
+fi
+# Generate a password.
+password() {
+	PASSWORD=$(date | sha256sum)
+}
+# Create the user.
+useradd -c "${2}" -m "${1}" >/dev/null
+# Check to see if the useradd command succeeded.
+if [[ "${?}" -ne 0 ]]
+then
+   	echo 'La creación de usuario ha sufrido un error'
+    exit 1
+fi
+password
+# Change the password.
+# Force password change on first login.
+echo -e $PASSWORD"\n"$PASSWORD | passwd $1 -e
+# Check to see if the passwd command succeeded.
+if [[ "${?}" -ne 0 ]]
+then
+  	echo 'La asignacion de contraseña ha sufrido un error'
+    exit 1
+fi
+# Display the username, password, and the host where the user was created.
+echo $1
+echo $PASSWORD
+echo $(hostname)
